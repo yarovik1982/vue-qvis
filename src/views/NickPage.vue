728500a6c682d961157a16ec8508ea82fcac8e45
acunamatata82 <yar.oleg1982@mail.ru>
2025-10-24 22:01:04 +0300

    lastFix

--- src/views/NickPage.vue
+++ src/views/NickPage.vue
@@ -3,9 +3,20 @@
 </script>
 <template>
     <h1 class="page-title">Для Коли</h1>
+    <div class="container">
+      <p>
+         Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты.
+      </p>
+    </div>
 </template>
 <style scoped >
 .page-title{
    text-align: center;
+}
+p{
+   padding: 20px;
+   border: 1px solid #000;
+   width: 500px;
+   margin: 0 auto;
 }
 </style>
